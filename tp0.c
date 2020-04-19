778b0a0ce264b11c5451681084267555b84b06a0
Sebastian Gilberto <sebastian.gilberto@despegar.com>
2020-04-19 20:22:34 -0300

    TP0 resuelto

--- tp0.c
+++ tp0.c
@@ -19,7 +19,7 @@ int main(void)
 
 	logger = iniciar_logger();
 
-	//Loggear "soy un log"
+	log_info(logger, "Soy un log!");
 
 	config = leer_config();
 
@@ -28,13 +28,26 @@ int main(void)
 
 	//antes de continuar, tenemos que asegurarnos que el servidor esté corriendo porque lo necesitaremos para lo que sigue.
 
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+
+	log_info(logger, "IP: %s", ip);
+	log_info(logger, "PUERTO: %s", puerto);
+
 	//crear conexion
+	conexion = crear_conexion(ip, puerto);
 
 	//enviar mensaje
+	enviar_mensaje("how you doing?", conexion);
 
 	//recibir mensaje
+	char *mensaje = recibir_mensaje(conexion);
 
 	//loguear mensaje recibido
+	log_info(logger, mensaje);
+
+	//libero mensaje
+	//free(mensaje);
 
 	terminar_programa(conexion, logger, config);
 }
@@ -42,17 +55,21 @@ int main(void)
 //TODO
 t_log* iniciar_logger(void)
 {
-
+	printf("\nCreando archivo de log");
+	return log_create("game-watch.log", "game-watch", true, LOG_LEVEL_INFO);
 }
 
 //TODO
-t_config* leer_config(void)
-{
-
+t_config* leer_config(void) {
+	printf("\nLeyendo archivo de configuración");
+	return config_create("tp0.config");
 }
 
 //TODO
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	//Y por ultimo, para cerrar, hay que liberar lo que utilizamos (conexion, log y config) con las funciones de las commons y del TP mencionadas en el enunciado
+	liberar_conexion(conexion);
+	log_destroy(logger);
+	config_destroy(config);
 }
--- utils.c
+++ utils.c
@@ -14,7 +14,16 @@
  */
 void* serializar_paquete(t_paquete* paquete, int *bytes)
 {
-
+	int size = sizeof(int) * 2 + strlen((char *) paquete->buffer->stream) + 1;
+	void *paquete_enviar = malloc(size);
+	*bytes = size;
+	int desplazamiento = 0;
+	memcpy(paquete_enviar + desplazamiento, &(paquete->codigo_operacion), sizeof(int));
+	desplazamiento += sizeof(int);
+	memcpy(paquete_enviar + desplazamiento, &(paquete->buffer->size), sizeof(int));
+	desplazamiento += sizeof(int);
+	memcpy(paquete_enviar + desplazamiento, paquete->buffer->stream, strlen((char *) paquete->buffer->stream) + 1);
+	return paquete_enviar;
 }
 
 int crear_conexion(char *ip, char* puerto)
@@ -42,13 +51,36 @@ int crear_conexion(char *ip, char* puerto)
 //TODO
 void enviar_mensaje(char* mensaje, int socket_cliente)
 {
-
+	printf("\nEnviando mensaje");
+	t_paquete *paquete = malloc(sizeof(t_paquete));
+	t_buffer *buffer = malloc(sizeof(t_buffer));
+	paquete->codigo_operacion = MENSAJE;
+	buffer->size = strlen(mensaje) + 1;
+	void *stream = malloc(strlen(mensaje) + 1);
+	stream = mensaje;
+	buffer->stream = stream;
+	paquete->buffer = buffer;
+	int size;
+	void *paquete_enviar = serializar_paquete(paquete, &size);
+	send(socket_cliente, paquete_enviar, size, 0);
+	printf("\nMensaje enviado");
+	free(paquete->buffer);
+	free(paquete);
+	free(paquete_enviar);
 }
 
 //TODO
 char* recibir_mensaje(int socket_cliente)
 {
-
+	printf("\nRecibiendo mensaje");
+	op_code cod_op;
+	recv(socket_cliente, &cod_op, sizeof(int), 0);
+	int size;
+	recv(socket_cliente, &size, sizeof(int), 0);
+	char *mensaje = malloc(size);
+	recv(socket_cliente, mensaje, size, 0);
+	printf("\nMensaje recibido");
+	return mensaje;
 }
 
 void liberar_conexion(int socket_cliente)
